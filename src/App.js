2b7f89a86de076e81330fb5e36d00498ede0d196
prakash-rawal <hello112000moto@gmail.com>
2023-09-29 11:08:40 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import React from 'react'
-import TilesGame from './game/TilesGame';
+
 import './todo.css';
 
-import TodoList from './component/TodoList';
 import TodoApp from './component/TodoList';
-
-
-// import TodoForm from './component/TodoForm';
 
 function App() {
   return (
     <>
-      {/* <TilesGame /> */}
-      {/* <TodoList /> */}
       <TodoApp />
     </>
   );
